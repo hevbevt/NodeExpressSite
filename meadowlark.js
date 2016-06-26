12f5b1524165be41361926ea590c925eefddf198
duanhe <duan.he@xinheyun.com>
2016-06-26 13:49:56 +0800

    test

--- meadowlark.js
+++ meadowlark.js
@@ -10,13 +10,6 @@ var handlebars = require('express3-handlebars')
         defaultLayout: 'main'
     });
 
-var fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-];
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -25,14 +18,18 @@ app.set('port', process.env.PORT || 3050);
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(function (req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+})
+
 app.get('/', function (req, res) {
     res.render('home');
 });
 
 app.get('/about', function (req, res) {
-    var randomFortune =fortunes[Math.floor(Math.random() * fortunes.length)];
     res.render('about', {
-        fortune: randomFortune
+        pageTestScript: '/qa/tests-about.js'
     });
 });
 
