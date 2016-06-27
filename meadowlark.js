765244b54ce674c939586ce5676415a25596fd5b
duanhe <duan.he@xinheyun.com>
2016-06-27 10:20:39 +0800

    qa

--- meadowlark.js
+++ meadowlark.js
@@ -21,7 +21,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(function (req, res, next) {
     res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
     next();
-})
+});
 
 app.get('/', function (req, res) {
     res.render('home');
@@ -31,6 +31,14 @@ app.get('/about', function (req, res) {
     res.render('about', {
         pageTestScript: '/qa/tests-about.js'
     });
+});
+
+app.get('/tours/hood-river', function (req, res) {
+    res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function (req, res) {
+    res.render('tours/request-group-rate');
 });
 
 //404
@@ -45,8 +53,6 @@ app.use(function (err, req, res, next) {
     res.status(500);
     res.render('500');
 });
-
-
 
 app.listen(app.get('port'), function () {
     console.log('Express started on localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
